df97ddae79710e89459aa062f8517d03022ba77e
Gwanwoo <rkdrhksdn2@nate.com>
2014-04-05 14:45:24 +0900

    bye~

--- javaScript/-study01/HelloWorld.js
+++
@@ -1,6 +0,0 @@
-/**
- * 
- */
-
-var str = "hello java scripts";
-alert(str);
--- javaScript/-study02/GuGuDan.js
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-for(var i=2;i<=9;i++){
-	for(var j=1;j<=9;j++){
-		console.log("구구단"+i+"x"+j+"="+i*j)
-	}
-}
