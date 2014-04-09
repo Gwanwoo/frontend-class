9dac83fcf59f5b518a7e425bcfb8e0a894fdc55d
Gwanwoo <rkdrhksdn2@nate.com>
2014-04-09 20:35:57 +0900

    report03DOMandRed

--- frontend-class/-study04/chessDOM.js
+++ frontend-class/-study04/chessDOM.js
@@ -5,18 +5,42 @@ var board = document.getElementById("board");
 
 var color;
 
-
-
 var sum;
-for(var i=0; i<4; i++){
-  for(var j=0; j<4; j++){
-    // TODO : 여기에 필요한 코드를 작성해주세요.
-    var   newSpan=document.createElement('span');
-    sum=0;
-    sum=i+j;
-    if(sum%2===1){color="white";}
-    else{color="black";}
-        newSpan.setAttribute('class',color);
-   board.appendChild(newSpan);
-  }
+var numberID = 1;
+for (var i = 0; i < 4; i++) {
+	for (var j = 0; j < 4; j++) {
+		// TODO : 여기에 필요한 코드를 작성해주세요.
+		var newSpan = document.createElement('span');
+		sum = i + j;
+
+		if (sum % 2 === 1) {
+			color = " white";
+		} else {
+			color = " black";
+		}
+		newSpan.setAttribute('id', sum);
+		newSpan.setAttribute('class', color);
+		board.appendChild(newSpan);
+		numberID++;
+
+	}
+
+};
+
+var paintRed = function(e) {
+
+	if (this.red != null) {
+		if ((this.red.id % 2) == 1) {
+			this.red.className = this.red.className.replace(" red", " white");
+			console.log(e.target.id);
+		} else if ((this.red.id % 2) == 0) {
+			this.red.className = this.red.className.replace(" red", " black");
+			console.log(e.target.id);
+		}
+	}
+	e.target.className += " red";
+	this.red = e.target;
+
 }
+var red = null;
+board.addEventListener('click', paintRed);
