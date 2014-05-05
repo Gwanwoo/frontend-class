d56f3279f5788e7f9a119c02438528795e7c0063
Gwanwoo <rkdrhksdn2@nate.com>
2014-05-05 13:53:13 +0900

    commit

--- frontend-class/-study06/pingpong.js
+++ frontend-class/-study06/pingpong.js
@@ -1,13 +1,14 @@
 /**
  * 
  */
+<script src="http://code.jquery.com/jquery-1.10.2.js"></script>
 var doc_h = (window.innerHeight || self.innerHeight
 		|| document.documentElement.clientHeight || document.body.clientHeight);
 var doc_w = (window.innerWidth || self.innerWidth
 		|| document.documentElement.clientWidth || document.body.clientWidth);
- //현재 브라우저의 최대 높이 최대 넓이를 구함.
+// 현재 브라우저의 최대 높이 최대 넓이를 구함.
 
-var MAX_hight_coordinate = (doc_h - 100) + "px"; 
+var MAX_hight_coordinate = (doc_h - 100) + "px";
 var MAX_width_coordinate = (doc_w - 100) + "px";
 var MIN_hight_coordinate = ((doc_h + 50) - (doc_h)) + "px";
 var MIN_width_coordinate = ((doc_w + 50) - (doc_w)) + "px";
